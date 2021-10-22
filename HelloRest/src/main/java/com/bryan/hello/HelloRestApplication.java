afe07ba1dd1a63e436b7668ad34b90833b1c3ac0
nanahjhna <nanahjhna7@naver.com>
2021-10-23 03:54:38 +0900

    first commit

---
+++ HelloRest/src/main/java/com/bryan/hello/HelloRestApplication.java
@@ -0,0 +1,13 @@
+package com.bryan.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRestApplication.class, args);
+	}
+
+}
